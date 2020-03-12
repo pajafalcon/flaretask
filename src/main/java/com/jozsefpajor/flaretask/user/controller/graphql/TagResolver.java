442df7937899c0be40a51b8f4c7f67c1ece88ccb
jopa <jozsef.pajor@inxmail.de>
2020-03-12 10:44:53 +0100

    Graphql works

--- src/main/java/com/jozsefpajor/flaretask/user/controller/graphql/TagResolver.java
+++
@@ -1,7 +0,0 @@
-//package com.jozsefpajor.flaretask.user.controller.graphql;
-//
-//import com.coxautodev.graphql.tools.GraphQLResolver;
-//import com.jozsefpajor.flaretask.user.controller.graphql.dto.TagDTO;
-//
-//public class TagResolver implements GraphQLResolver<TagDTO> {
-//}
